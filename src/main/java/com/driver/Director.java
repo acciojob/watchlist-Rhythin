4a08bac92c08ec40ff5654a891cddb767fe847b4
Rhythin <rhythin13021996@gmail.com>
2023-01-26 19:20:50 +0530

    first solution postman testing success

---
+++ src/main/java/com/driver/Director.java
@@ -0,0 +1,47 @@
+package com.driver;
+
+public class Director {
+
+    private String name;
+    private int numberOfMovies;
+    private double imdbRating;
+
+    //all args constructor
+    public Director(String name, int numberOfMovies, double imdbRating) {
+        this.name = name;
+        this.numberOfMovies = numberOfMovies;
+        this.imdbRating = imdbRating;
+    }
+
+    //no args constructor
+    public Director() {
+
+    }
+
+    //getter and setters for all variables
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfMovies() {
+        return numberOfMovies;
+    }
+
+    public void setNumberOfMovies(int numberOfMovies) {
+        this.numberOfMovies = numberOfMovies;
+    }
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+}
---
+++ src/main/java/com/driver/Movie.java
@@ -0,0 +1,47 @@
+package com.driver;
+
+public class Movie {
+
+    private String name;
+    private int durationInMinutes;
+    private double imdbRating;
+
+    //all args constructor
+    public Movie(String name, int durationInMinutes, double imdbRating) {
+        this.name = name;
+        this.durationInMinutes = durationInMinutes;
+        this.imdbRating = imdbRating;
+    }
+
+    //noa args constructor
+    public Movie() {
+
+    }
+
+    //getter and setter functions for all variables
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(int imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+}
